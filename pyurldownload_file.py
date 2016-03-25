48c21a1223165bb7a5aea4675ec455abd3d13f5f
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-03-25 18:10:05 -0430

    Fixed http error and change urldownload

--- pyurldownload_file.py
+++ pyurldownload_file.py
@@ -5,12 +5,13 @@ from bs4 import *
 import sys 
 import wget
 
-url  = raw_input('Enter URL -> ')
-pattern = raw_input('Enter search pattern-> ')
+
+url  = input('Enter URL -> ')
+pattern = input('Enter search pattern-> ')
 
 html = requests.get(url)
 
-
+dir_download = "./download/"
 
 
 if html.text.find("400 Bad Request") != -1:
@@ -22,13 +23,20 @@ soup = BeautifulSoup(html.text)
 tags = soup('a')
 
 for tag in tags:
-	urldownload = "http://www.asambleanacional.gob.ve/"
-	url = tag.get('href',None)
-	text = str(url)
+	url_path = tag.get('href',None)
+	text = str(url_path)
 	if text.find(pattern) == -1: continue
-	urldownload +=  text
+	domain = url.split("http://")[1].split("/")[0]
+	urldownload = "http://" + domain + text
 	print ("Retrieve: {0},{1}".format(tag.contents[0],urldownload))
 	file = 	text.split("/")[-1]
 	path_and_file = dir_download + file
-	wget.download(urldownload)		
-	
+	try:
+		r = requests.get(urldownload)
+		with open(path_and_file, "wb") as f:
+			f.write(r.content)
+	except ConnectionError:
+		print("I can't download file: {0}".format(file))
+	except HTTPError:
+		print("I can't download file: {0}".format(file))
+	f.close()
